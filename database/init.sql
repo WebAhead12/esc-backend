27122b039f536c2252c864454da5448575ff7dad
George Joubran <durd2001@gmail.com>
2021-12-14 16:33:42 +0200

    server setup with router

---
+++ database/init.sql
@@ -0,0 +1,43 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users,teams,request,invite CASCADE;
+
+CREATE TABLE players(
+    id SERIAL PRIMARY KEY,
+    username varchar(36),
+    password varchar(255),
+    firstname varchar(255),
+    lastname varchar(255),
+    email varchar(255),
+    age INTEGER,
+    languages varchar(255),
+    gender varchar(255),
+    imagelink varchar(255),
+    location varchar(255),
+    registerdate DATE DEFAULT CURRENT_TIMESTAMP,
+    stats json
+);
+
+CREATE TABLE teams(
+    id SERIAL PRIMARY KEY,
+    teamname varchar(36),
+    password varchar(255),
+    email varchar(255),
+    description varchar(255),
+    imagelink varchar(255),
+    players json,
+    games json
+)
+
+CREATE TABLE requests(
+    playerid INTEGER REFERENCES players(id),
+    teamid INTEGER REFERENCES teams(id),
+    content json,
+    status varchar(12)
+)
+
+CREATE TABLE invites(
+    playerid INTEGER REFERENCES players(id),
+    teamid INTEGER REFERENCES teams(id),
+
+)
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = require("./router");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
